043b42070bbbee5485946ef85faed97b821d7181
Pedro Monteiro <pedromont436@gmail.com>
2024-03-19 22:20:58 -0300

    primeiro commit

---
+++ src/test/java/com/pedromonteiro/picpaydesafiobackend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromonteiro.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
