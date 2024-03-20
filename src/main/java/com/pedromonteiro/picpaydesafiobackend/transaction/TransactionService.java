9c8d3e71f72e473ea40a51c9a97461f518746e01
Pedro Monteiro <pedromont436@gmail.com>
2024-03-20 00:58:25 -0300

    criando metodo para validar transaçoes nos services

--- src/main/java/com/pedromonteiro/picpaydesafiobackend/transaction/TransactionService.java
+++ src/main/java/com/pedromonteiro/picpaydesafiobackend/transaction/TransactionService.java
@@ -1,8 +1,56 @@
 package com.pedromonteiro.picpaydesafiobackend.transaction;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
+
+import com.pedromonteiro.picpaydesafiobackend.wallet.Wallet;
+import com.pedromonteiro.picpaydesafiobackend.wallet.WalletRepository;
+import com.pedromonteiro.picpaydesafiobackend.wallet.WalletType;
 
 @Service
 public class TransactionService {
+    
+    private final TransactionRepository transactionRepository;
+    private final WalletRepository walletRepository;
+
+    public TransactionService(TransactionRepository transactionRepository, WalletRepository walletRepository) {
+        this.transactionRepository = transactionRepository;
+        this.walletRepository = walletRepository;
+    }
+
+    public Transaction create(Transaction transaction) {
+        // validar
+        
+
+        //criar transacao
+        var newTransaction = transactionRepository.save(transaction);
+
+        //debitar da carteira
+        var wallet = walletRepository.findById(transaction.payer()).get();
+        walletRepository.save(wallet.debit(transaction.value()));
+
+        return newTransaction;
+    }
+
+    /*
+     * the payer has a common wallet
+     * the payer has enough balance
+     * the payer is not the payee
+     */
+    private void validate(Transaction transaction) {
+        walletRepository.findById(transaction.payee())
+        .map(payee -> walletRepository.findById(transaction.payer())
+            .map(
+                payer -> isTransactionValid(transaction, payer))
+            .orElseThrow())
+        .orElseThrow();
+  }
+
+    private Boolean isTransactionValid(Transaction transaction, Wallet payer) {
+        return payer.type() == WalletType.COMUM &&
+            payer.balance().compareTo(transaction.value()) >= 0 &&
+            !payer.id().equals(transaction.payee()) ? true : null;
+    }
     
 }
--- src/main/java/com/pedromonteiro/picpaydesafiobackend/wallet/Wallet.java
+++ src/main/java/com/pedromonteiro/picpaydesafiobackend/wallet/Wallet.java
@@ -10,8 +10,11 @@ public record Wallet(
     Long cpf,
     String email,
     String password,
-    int type,
+    WalletType type,
     BigDecimal balance
 ) {
     
+    public Wallet debit(BigDecimal value) {
+        return new Wallet(id, fullName, cpf, email, password, type, balance.subtract(value));
+    }
 }
